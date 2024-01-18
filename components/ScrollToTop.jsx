f71b42535a1a15ef6527a343022cb391e397b736
furkantaglik <furkantaglik2334@gmail.com>
2024-01-18 17:53:20 +0300

    .

---
+++ components/ScrollToTop.jsx
@@ -0,0 +1,14 @@
+// "use client";
+// import { useEffect } from "react";
+// import { usePathname } from "next/navigation";
+
+// const ScrollToTop = () => {
+//   const path = usePathname();
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
+//   }, [path]);
+
+//   return null;
+// };
+
+// export default ScrollToTop;
